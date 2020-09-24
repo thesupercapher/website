0ce17b0fa9e0c1eda578372a296d641191759652
thesupercapher <71511337+thesupercapher@users.noreply.github.com>
2020-09-25 01:04:37 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var closebuttons = document.getElementsByClassName("close");
-var i;
-
-for(i = 0; i < closebuttons.length; i++) {
-    closebuttons[i].addEventListener("click", function () {
-        this.parentElement.style.display = "none";
-    });
-}
